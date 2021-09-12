f59bbcd72bff19d23c2256cf76a523d03a48f4e0
Yurii <leoonovn@gmail.com>
2021-09-12 13:49:16 +0300

    Сделал сущности БД

--- Modul4HW6/Modul4HW6/Entities/Artist.cs
+++ Modul4HW6/Modul4HW6/Entities/Artist.cs
@@ -8,5 +8,12 @@ namespace Modul4HW6.Entities
 {
     public class Artist
     {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual DateTime DateOfBirth { get; set; }
+        public virtual string Phone { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string InstagramUrl { get; set; }
+        public virtual List<Song> Songs { get; set; } = new List<Song>();
     }
 }
---
+++ Modul4HW6/Modul4HW6/Entities/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4HW6.Entities
+{
+    public class Song
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual double Duration { get; set; }
+        public virtual DateTime ReleasedDate { get; set; }
+        public virtual int GanreId { get; set; }
+        public virtual Ganre Ganre { get; set; }
+        public virtual List<Artist> Artists { get; set; } = new List<Artist>();
+    }
+}
